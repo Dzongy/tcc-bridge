69a0bf76b6d44ef46c8641746a56ef3d31cea84e
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-18 18:48:37 -0700

    BRIDGE V2: Termux:Boot script

--- boot-bridge.sh
+++ boot-bridge.sh
@@ -1,4 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/sh
-# Termux:Boot script for TCC Bridge
+#!/data/data/com.termux/files/usr/bin/bash
 termux-wake-lock
-pm2 resurrect || pm2 start ecosystem.config.js
+pm2 resurrect
+cloudflared tunnel run 18ba1a49-fdf9-4a52-a27a-5250d397c5c5 &
