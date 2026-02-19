1be5e80db6ec8b36ae4ce29a77af7d2d10e205ce
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-18 18:44:11 -0700

    Bridge V6: Boot Persistence

--- termux-boot.sh
+++ termux-boot.sh
@@ -1,13 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# TCC Bridge â Bulletproof Auto-Start
-# This script goes in ~/.termux/boot/
-
-# Wait for network
-sleep 10
-
-# Resurrect PM2 processes (Bridge + Tunnel)
-export PATH=$PATH:/data/data/com.termux/files/usr/bin
+# TCC Bridge Boot Persistence
+sleep 15
+termux-wake-lock
 pm2 resurrect
-
-# Send notification
-curl -d "Bridge V2 Resurrected after Reboot" https://ntfy.sh/tcc-zenith-hive
