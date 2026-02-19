d5231f265ce878a61bb77d4d22e789e6bd871909
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-18 18:37:54 -0700

    Bridge V2: Persistent boot script

--- termux-boot.sh
+++ termux-boot.sh
@@ -1,10 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# TCC Bridge â Bulletproof Auto-Start
+# Location: ~/.termux/boot/start-tcc.sh
+# Requires Termux:Boot app
+sleep 15
 termux-wake-lock
-sleep 20
-pm2 resurrect || {
-  cd ~/tcc-bridge
-  pm2 start ecosystem.config.js
-  pm2 save
-}
-curl -d "Bridge v6 Resurrected" https://ntfy.sh/tcc-zenith-hive
+pm2 resurrect
