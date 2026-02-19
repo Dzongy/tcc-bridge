a2ca0a386bec6cf6962369a11410781c545aff1b
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-18 18:57:08 -0700

    PM2: Ecosystem config with cloudflared

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,24 +4,24 @@ module.exports = {
       name: "bridge",
       script: "bridge.py",
       interpreter: "python3",
-      restart_delay: 5000,
-      env: { BRIDGE_PORT: "8080" }
-    },
-    {
-      name: "amos",
-      script: "amos.js",
       restart_delay: 5000
     },
     {
-      name: "state-push",
-      script: "state-push.py",
-      interpreter: "python3",
+      name: "tunnel",
+      script: "cloudflared",
+      args: "tunnel run 18ba1a49-fdf9-4a52-a27a-5250d397c5c5",
       restart_delay: 10000
     },
     {
       name: "watchdog",
       script: "watchdog-v2.sh",
       interpreter: "bash"
+    },
+    {
+      name: "state-push",
+      script: "state-push.py",
+      interpreter: "python3",
+      restart_delay: 10000
     }
   ]
 };
