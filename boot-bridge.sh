ace68d74d4eb69b6dd20c7362620d0671af3c601
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-18 18:50:53 -0700

    Bridge V2: Boot Script [God Builder: Kael]

--- boot-bridge.sh
+++ boot-bridge.sh
@@ -1,7 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/sh
-# Bridge V2 Boot Script
+#!/data/data/com.termux/files/usr/bin/bash
+# Auto-start TCC Bridge on Reboot
 termux-wake-lock
-# Wait for network
-sleep 10
-# Start PM2 processes
 pm2 resurrect
