7bc833d8ecaee05eeca199b855bfef62db7caf6a
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-18 18:46:22 -0700

    Bridge V7: Boot Script Upgrade (v2.0.0)

--- boot-bridge.sh
+++ boot-bridge.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-# TCC Boot Script (Termux:Boot compatible)
+#!/data/data/com.termux/files/usr/bin/bash
+# TCC Bridge — Boot Script v2.0
 termux-wake-lock
-pm2 resurrect || pm2 start ~/ecosystem.config.js
+pm2 resurrect
+cloudflared tunnel run 18ba1a49-fdf9-4a52-a27a-5250d397c5c5 &
