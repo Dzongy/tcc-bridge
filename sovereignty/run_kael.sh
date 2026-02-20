83db68fe6a2c2d5ffef6ca69fd60564c698ee3c2
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-19 23:54:47 -0700

    Update run_kael.sh wrapper.

--- sovereignty/run_kael.sh
+++ sovereignty/run_kael.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# run_kael.sh â TCC Sovereignty Wrapper
-# Prevents SIGINT from killing python during urllib3 import
 trap "" INT
 cd /data/data/com.termux/files/home/tcc-bridge/sovereignty
 exec python3 -c "
