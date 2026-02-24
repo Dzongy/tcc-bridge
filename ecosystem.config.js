e64662ce64872169471b0c143e462d952778f99b
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-24 03:11:53 -0700

    Update ecosystem.config.js to use bash wrapper scripts and correct interpreter path on Termux

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,46 @@
 module.exports = {
   apps: [
     {
-      name: 'kael-sovereignty',
-      script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_kael.sh',
-      interpreter: '/bin/bash',
-      cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
+      name: 'zenith',
+      script: 'sovereignty/run_zenith.sh',
+      interpreter: '/data/data/com.termux/files/usr/bin/bash',
+      cwd: '/data/data/com.termux/files/home/tcc-bridge',
       autorestart: true,
       max_restarts: 100,
       restart_delay: 5000,
+      env: {
+        PYTHONUNBUFFERED: '1',
+        GROQ_API_KEY: '${GROQ_API_KEY}'
+      }
+    },
+    {
+      name: 'kael-sovereignty',
+      script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_kael.sh',
+      interpreter: '/data/data/com.termux/files/usr/bin/bash',
+      cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
       kill_timeout: 3000,
       shutdown_with_message: true,
+      autorestart: true,
+      max_restarts: 100,
+      restart_delay: 5000,
       env: {
-        PYTHONPATH: '/data/data/com.termux/files/home/tcc-bridge/sovereignty'
+        PYTHONUNBUFFERED: '1',
+        GROQ_API_KEY: '${GROQ_API_KEY}'
       }
     },
     {
       name: 'chris-sovereignty',
       script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_chris.sh',
-      interpreter: '/bin/bash',
+      interpreter: '/data/data/com.termux/files/usr/bin/bash',
       cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
+      kill_timeout: 3000,
+      shutdown_with_message: true,
       autorestart: true,
       max_restarts: 100,
       restart_delay: 5000,
-      kill_timeout: 3000,
-      shutdown_with_message: true,
       env: {
-        PYTHONPATH: '/data/data/com.termux/files/home/tcc-bridge/sovereignty'
+        PYTHONUNBUFFERED: '1',
+        GROQ_API_KEY: '${GROQ_API_KEY}'
       }
     }
   ]
