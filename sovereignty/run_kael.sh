a0fa495721c51dbf61156df560085a524efe2302
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-24 03:13:08 -0700

    Fix: Robust bash wrapper for Kael with SIGINT protection

--- sovereignty/run_kael.sh
+++ sovereignty/run_kael.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# TCC Sovereignty Wrapper with SIGINT Protection
 trap "" INT
 cd /data/data/com.termux/files/home/tcc-bridge/sovereignty
-exec python3 -c "
+# Use python3 -c to ignore SIGINT before any imports happen
+exec python3 -u -c "
 import signal
 signal.signal(signal.SIGINT, signal.SIG_IGN)
 import runpy
