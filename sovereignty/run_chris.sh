94d56b0fb042a568b5ccce63470d1c6ffb8362fd
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-24 03:13:14 -0700

    fix(sovereignty): resolve SIGINT crash loop with bash wrappers (run_chris) v2

--- sovereignty/run_chris.sh
+++ sovereignty/run_chris.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# TCC Sovereignty Wrapper with SIGINT Protection
 trap "" INT
 cd /data/data/com.termux/files/home/tcc-bridge/sovereignty
-# Use python3 -c to ignore SIGINT before any imports happen
-exec python3 -u -c "
+exec python3 -c "
 import signal
 signal.signal(signal.SIGINT, signal.SIG_IGN)
 import runpy
