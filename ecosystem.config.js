2347591103e358afd4981c6c3b9b436fefafe456
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-24 03:14:47 -0700

    Fix: Updated ecosystem.config.js with kill_timeout and shutdown_with_message (Mission Step 3)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,29 +3,31 @@ module.exports = {
     {
       name: 'kael-sovereignty',
       script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_kael.sh',
-      interpreter: '/bin/bash',
+      interpreter: '/data/data/com.termux/files/usr/bin/bash',
       cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
+      kill_timeout: 3000,
+      shutdown_with_message: true,
       autorestart: true,
       max_restarts: 100,
       restart_delay: 5000,
-      kill_timeout: 3000,
-      shutdown_with_message: true,
       env: {
-        PYTHONPATH: '.'
+        PYTHONUNBUFFERED: '1',
+        GROQ_API_KEY: '${GROQ_API_KEY}'
       }
     },
     {
       name: 'chris-sovereignty',
       script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_chris.sh',
-      interpreter: '/bin/bash',
+      interpreter: '/data/data/com.termux/files/usr/bin/bash',
       cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
+      kill_timeout: 3000,
+      shutdown_with_message: true,
       autorestart: true,
       max_restarts: 100,
       restart_delay: 5000,
-      kill_timeout: 3000,
-      shutdown_with_message: true,
       env: {
-        PYTHONPATH: '.'
+        PYTHONUNBUFFERED: '1',
+        GROQ_API_KEY: '${GROQ_API_KEY}'
       }
     }
   ]
