7927c00adbac01efcf1571817b10ac314d11bcf0
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-24 03:35:50 -0700

    fix: add zenith-sovereignty pm2 entry

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   apps: [
     {
+      name: 'zenith-sovereignty',
+      script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_zenith.sh',
+      interpreter: '/bin/bash',
+      cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
+      autorestart: true,
+      max_restarts: 100,
+      restart_delay: 5000,
+      kill_timeout: 5000,
+      wait_ready: true,
+      listen_timeout: 10000,
+      shutdown_with_message: true,
+      treekill: false,
+      env: {
+        PYTHONPATH: '/data/data/com.termux/files/home/tcc-bridge/sovereignty'
+      }
+    },
+    {
       name: 'kael-sovereignty',
       script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_kael.sh',
       interpreter: '/bin/bash',
