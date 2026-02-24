b363042a72cadbee3eda070d9d1d82a2fd477ee3
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-24 03:12:16 -0700

    Fix: Robust bash wrapper for Zenith with SIGINT protection

--- sovereignty/run_zenith.sh
+++ sovereignty/run_zenith.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# TCC Sovereignty Wrapper with SIGINT Protection
 trap "" INT
 cd /data/data/com.termux/files/home/tcc-bridge/sovereignty
-exec python3 -u zenith_core.py
+# Use python3 -c to ignore SIGINT before any imports happen
+exec python3 -u -c "
+import signal
+signal.signal(signal.SIGINT, signal.SIG_IGN)
+import runpy
+runpy.run_path('zenith_core.py', run_name='__main__')
+"
