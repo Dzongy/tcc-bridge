a4c8c7f40ad40803976bf6092cfe50257a1a46bb
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-24 03:12:47 -0700

    Fix: SIGINT protection and PM2 settings in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,21 +2,23 @@ module.exports = {
   apps: [
     {
       name: 'zenith',
-      script: 'sovereignty/run_zenith.sh',
-      interpreter: '/data/data/com.termux/files/usr/bin/bash',
-      cwd: '/data/data/com.termux/files/home/tcc-bridge',
+      script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_zenith.sh',
+      interpreter: '/bin/bash',
+      cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
+      kill_timeout: 3000,
+      shutdown_with_message: true,
       autorestart: true,
       max_restarts: 100,
       restart_delay: 5000,
       env: {
         PYTHONUNBUFFERED: '1',
-        GROQ_API_KEY: '${GROQ_API_KEY}'
+        GROQ_API_KEY: process.env.GROQ_API_KEY
       }
     },
     {
       name: 'kael-sovereignty',
       script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_kael.sh',
-      interpreter: '/data/data/com.termux/files/usr/bin/bash',
+      interpreter: '/bin/bash',
       cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
       kill_timeout: 3000,
       shutdown_with_message: true,
@@ -25,13 +27,13 @@ module.exports = {
       restart_delay: 5000,
       env: {
         PYTHONUNBUFFERED: '1',
-        GROQ_API_KEY: '${GROQ_API_KEY}'
+        GROQ_API_KEY: process.env.GROQ_API_KEY
       }
     },
     {
       name: 'chris-sovereignty',
       script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_chris.sh',
-      interpreter: '/data/data/com.termux/files/usr/bin/bash',
+      interpreter: '/bin/bash',
       cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
       kill_timeout: 3000,
       shutdown_with_message: true,
@@ -40,8 +42,8 @@ module.exports = {
       restart_delay: 5000,
       env: {
         PYTHONUNBUFFERED: '1',
-        GROQ_API_KEY: '${GROQ_API_KEY}'
+        GROQ_API_KEY: process.env.GROQ_API_KEY
       }
     }
   ]
-};
+}
