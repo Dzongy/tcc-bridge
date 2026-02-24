b11c53bedbc85ad490dbb67bf0858c073408bac2
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-24 03:09:07 -0700

    Update ecosystem.config.js to use SIGINT-safe wrapper scripts for sovereignty agents

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,32 @@
-{{package.json}}
+module.exports = {
+  apps: [
+    {
+      name: 'kael-sovereignty',
+      script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_kael.sh',
+      interpreter: '/bin/bash',
+      cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
+      autorestart: true,
+      max_restarts: 100,
+      restart_delay: 5000,
+      kill_timeout: 3000,
+      shutdown_with_message: true,
+      env: {
+        PYTHONPATH: '/data/data/com.termux/files/home/tcc-bridge/sovereignty'
+      }
+    },
+    {
+      name: 'chris-sovereignty',
+      script: '/data/data/com.termux/files/home/tcc-bridge/sovereignty/run_chris.sh',
+      interpreter: '/bin/bash',
+      cwd: '/data/data/com.termux/files/home/tcc-bridge/sovereignty',
+      autorestart: true,
+      max_restarts: 100,
+      restart_delay: 5000,
+      kill_timeout: 3000,
+      shutdown_with_message: true,
+      env: {
+        PYTHONPATH: '/data/data/com.termux/files/home/tcc-bridge/sovereignty'
+      }
+    }
+  ]
+};
