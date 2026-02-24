2c4beab72ad88cef1851e12aabf38eb3ee072815
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-24 03:06:07 -0700

    Fix: Use setsid and signal ignore in run_kael.sh wrapper to survive PM2 SIGINT on startup

--- sovereignty/run_kael.sh
+++ sovereignty/run_kael.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 trap "" INT
 cd /data/data/com.termux/files/home/tcc-bridge/sovereignty
-exec python3 -c "
+# Use setsid to run python in a new session, detaching from the caller's process group
 import signal
 signal.signal(signal.SIGINT, signal.SIG_IGN)
 import runpy
