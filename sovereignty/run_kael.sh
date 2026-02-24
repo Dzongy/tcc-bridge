4044acee227474d2963f757658229a104d525c79
Dzongy <94261390+Dzongy@users.noreply.github.com>
2026-02-24 03:37:14 -0700

    Add run_kael.sh wrapper to ignore SIGINT during startup

--- sovereignty/run_kael.sh
+++ sovereignty/run_kael.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Resolve SIGINT crash loop on Termux Python 3.12
 trap "" INT
 cd /data/data/com.termux/files/home/tcc-bridge/sovereignty
-exec setsid python3 -c "import signal; signal.signal(signal.SIGINT, signal.SIG_IGN); import runpy; runpy.run_path('agent_core.py', run_name='__main__')"
+exec python3 -c "
+import signal
+signal.signal(signal.SIGINT, signal.SIG_IGN)
+import runpy
+runpy.run_path('agent_core.py', run_name='__main__')
+"
